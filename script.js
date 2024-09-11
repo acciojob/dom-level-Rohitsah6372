fc361499eca39fea6cc4a49f0bfb15edad8278a1
Rohitsah6372 <91113994+Rohitsah6372@users.noreply.github.com>
2024-09-11 21:51:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const targetLevel = document.getElementById("level")
+
+
+function getDomainLevel(element){
+	int level =0;
+	while(element){
+		level++;
+		break;
+	}
+	return level;
+}
+
+const domLevel = getDomainLevel(targetLevel);
